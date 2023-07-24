a9c1fe1358bb9e2d8db61283bd175d4ed8fe069a
Jerricke <eejerrickw@gmail.com>
2023-07-24 14:12:10 -0600

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    even = list();
+    for num in num_list:
+        if num%2 == 0:
+            even.append(num)
+    return even
     pass
 
 def make_exclamation(sentence_list):
+    newArray = list()
+    for index in sentence_list:
+        newArray.append(index + "!")
+        print(newArray)
+    return newArray
     pass
